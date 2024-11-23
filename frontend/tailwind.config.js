30f7cfaba9a2d7f94ee4142a4d56b062dedd9ae6
Diogo Gaspar <diogo.gaspar228@gmail.com>
2024-11-23 17:08:45 +0000

    add colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,8 +2,30 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      colors: {
+        white: {
+          DEFAULT: '#ffffff'
+        },
+        black: {
+          DEFAULT: '#000000'
+        },
+        gray: {
+          DEFAULT: '#E9E9E9',
+          dark: '#B6B6B6'
+        },
+        blue: {
+          DEFAULT: '#0077CC',
+          dark: '#005C99'
+        },
+        red: {
+          DEFAULT: '#FF0000',
+          dark: '#CC0000'
+        },
+      },
+    },
+    plugins: [],
+  }
 }
+
 
